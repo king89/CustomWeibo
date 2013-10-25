34b8cfacd8f160a1a7000267dfc2cffef94e8c74
king <liangqz89@gmail.com>
2013-10-25 21:57:03 +0800

    add index

--- src/CustomWeibo/settings.py
+++ src/CustomWeibo/settings.py
@@ -1,7 +1,10 @@
 # Django settings for CustomWeibo project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_ROOT = os.path.dirname(__file__)
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -111,6 +114,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
